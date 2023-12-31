cc8a8e6bb139cd94b428253d81d93a93dc8968c9
thong.bui <thongbui97@gmail.com>
2024-01-01 01:22:16 +0700

    feat: LeetCode 75

--- src/Prob1657.java
+++ src/Prob1657.java
@@ -1,6 +1,9 @@
 public class Prob1657 {
 	public boolean closeStrings(String word1, String word2) {
-		
+        if (word1.length() != word2.length()) {
+            return false;
+        }
+
        return false; 
     }	
 }
